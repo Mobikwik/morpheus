1330826b26b0f20c5bfead17387b608e6c99a061
chirag-mobi <chirag@mobikwik.com>
2019-10-18 15:56:22 +0530

    shell script to run the application

---
+++ goMorpheus.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/home/chiragjain/mobikwik/softwares/go1.12.5.linux-amd64/go/bin
+export GOPATH=/home/chiragjain/mobikwik/codebase/goLangProjects
+export CGO_ENABLED=0
+go run morpheusMain.go mockedResponseBodyProcessor.go apiJsonConfigHandler.go mockedResponseProcessor.go variableJsonConfigHandler.go mockingRequestHandler.go mockedResponseHeaderProcessor.go commonTestFunctions.go &
