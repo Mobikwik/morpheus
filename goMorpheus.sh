01f1e6000fd996c635e1fa4c58085c1610d6bcbb
chirag-mobi <chirag@mobikwik.com>
2020-02-19 18:36:44 +0530

    Update goMorpheus.sh

--- goMorpheus.sh
+++ goMorpheus.sh
@@ -1,5 +1,5 @@
-export PATH=$PATH:/home/chiragjain/mobikwik/softwares/go1.12.5.linux-amd64/go/bin
-export GOPATH=/home/chiragjain/mobikwik/codebase/goLangProjects
+export PATH=$PATH:/home/user/go1.12.5.linux-amd64/go/bin
+export GOPATH=/home/user/goLangProjects
 export CGO_ENABLED=0
 go run $(ls -t | grep -v _test | grep .go) env.properties > /tmp/morpheusLog.log 2>&1 &
 
